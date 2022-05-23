a971639682e3f292a6b01a6a0524bc6b4375ca64
Kristijan Baro <baro.kristijan@gmail.com>
2022-05-23 18:27:39 +0200

    sinished product, added js ofr table

---
+++ orders.js
@@ -0,0 +1,33 @@
+const Orders = [
+    {
+        productName: 'Something',
+        productNumber: '8282',
+        paymentStatus:  'Due',
+        shipping: 'Pending'
+
+    },
+    {
+        productName: 'Something 2',
+        productNumber: '4568',
+        paymentStatus:  'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Something 3',
+        productNumber: '2358',
+        paymentStatus:  'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Something 4',
+        productNumber: '5686',
+        paymentStatus:  'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Something 5',
+        productNumber: '7855',
+        paymentStatus:  'Refunded',
+        shipping: 'Declined'
+    }
+]
